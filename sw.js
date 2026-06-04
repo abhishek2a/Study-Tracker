b5d2f9264ab31bae40b44c4f83fcbbf5d80ab11a
abhishek2a <abhishekoac10@gmail.com>
2026-06-04 14:57:11 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'study-tracker-v1.2.7';
+const CACHE_NAME = 'study-tracker-v1.2.8';
 
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  './icon.svg'
+  './manifest.json?v=1.2.8',
+  './icon.svg?v=1.2.8'
 ];
 
 self.addEventListener('message', event => {
