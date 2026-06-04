8d65ed27b4d4457465c4a9845042955e9d1c176d
abhishek2a <abhishekoac10@gmail.com>
2026-06-04 11:29:40 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,8 +6,13 @@ const ASSETS = [
   './manifest.json'
 ];
 
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
