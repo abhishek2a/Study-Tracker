a0c033fabdc7ae200b437bd81900fe81f3481d4f
Abhishek S <abhishekoac10@gmail.com>
2026-08-03 12:39:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'study-tracker-v1.2.90-log-session28';
+const CACHE = 'study-tracker-v1.2.90-log-session29';
 const PRECACHE = ['/', './index.html', './manifest.json', './logo.svg', './logo.ico'];
 
 self.addEventListener('install', e => {
