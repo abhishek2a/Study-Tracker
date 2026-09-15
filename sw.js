5c61e7c077ddd62acb98f35eb9118f6943618f8d
Abhishek S <abhishekoac10@gmail.com>
2026-09-15 23:33:12 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'study-tracker-v1.5.5-offline-bandwidth-opt';
+const CACHE = 'study-tracker-v1.5.6-offline-bandwidth-opt';
 const PRECACHE = [
   './index.html', 
   './manifest.json', 
